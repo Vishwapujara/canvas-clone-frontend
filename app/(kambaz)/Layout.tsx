86baa26cd87da47928149a93d439b6f6594680eb
Vishwapujara <vishwapujara.08@gmail.com>
2025-11-07 18:33:51 -0500

    assignmet 4(6)

--- app/(kambaz)/Layout.tsx
+++ app/(kambaz)/Layout.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 import { ReactNode } from "react";
 import KambazNavigation from "./Navigation";
